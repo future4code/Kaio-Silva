ebf9b91f01667211435f2f5e0a48dd7c705914c1
kaio vicente <kaio_vicente09@hotmail.com>
2021-08-16 00:41:49 -0300

    como eu não consegui fazer os exercicios 7, 10 e 11, eu olhei nas sugestões, fiquei analisando até entender, e coloquei no meu projeto

--- quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
+++ quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -1,82 +1,89 @@
 // EXERCÍCIO 01
-  function inverteArray(array) {
-    console.log(array)
-   let tamanhoDoArray = array.length
-    for(let i = tamanhoDoArray - 1; i >= 0; i--){
-          array.push(array[i])
-    }  
-    array.splice(0,array.length /2)
-    console.log(array) 
-    return array
-  } 
-  // inverteArray()
-  
+function inverteArray(array) {
+  console.log(array)
+  let tamanhoDoArray = array.length
+  for (let i = tamanhoDoArray - 1; i >= 0; i--) {
+    array.push(array[i])
+  }
+  array.splice(0, array.length / 2)
+  console.log(array)
+  return array
+}
+inverteArray()
+
 
 // EXERCÍCIO 02
 function retornaNumerosParesElevadosADois(array) {
- novo =[]
- 
- for(let i=0;i<array.length;i++){
-   if(array[i] %2 ===0){
-     novo.push(array[i] * array[i]) 
-   }
-   
- }
- return novo
- 
-} 
-// retornaNumerosParesElevadosADois()
+  novo = []
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      novo.push(array[i] * array[i])
+    }
+
+  }
+  return novo
+
+}
+retornaNumerosParesElevadosADois()
 
 // EXERCÍCIO 03
 function retornaNumerosPares(array) {
- pares=[]
- for(let i =0;i<array.length;i++){
-   if(array[i] %2 ===0){
-     pares.push(array[i])
-   }
- }
- return pares
+  pares = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      pares.push(array[i])
+    }
+  }
+  return pares
 }
 
-// retornaNumerosPares()
+retornaNumerosPares()
 
 // EXERCÍCIO 04
 function retornaMaiorNumero(array) {
-   let maiorNumero = 0
-  for(let i = 0;i<array.length;i++){
-    if(array[i] > maiorNumero){
+  let maiorNumero = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maiorNumero) {
       maiorNumero = array[i]
     }
 
   }
-return maiorNumero
+  return maiorNumero
 }
-// retornaMaiorNumero()
+retornaMaiorNumero()
 
 // EXERCÍCIO 05
 function retornaQuantidadeElementos(array) {
   return array.length
 }
-// retornaQuantidadeElementos()
+retornaQuantidadeElementos()
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
   const booleano1 = true
   const booleano2 = false
-  const booleano3 = !booleano2 
+  const booleano3 = !booleano2
   const booleano4 = !booleano3
 
-  const respostas =[booleano1 && booleano2 && !booleano4, (booleano1 && booleano2) || !booleano3,
-    (booleano2 || booleano3) && (booleano4 || booleano1), !(booleano2 && booleano3) || !(booleano1 && booleano3),
-    !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)]
+  const respostas = [booleano1 && booleano2 && !booleano4, (booleano1 && booleano2) || !booleano3,
+  (booleano2 || booleano3) && (booleano4 || booleano1), !(booleano2 && booleano3) || !(booleano1 && booleano3),
+  !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)]
 
-    return respostas
+  return respostas
 }
-// retornaExpressoesBooleanas()
+retornaExpressoesBooleanas()
 
 // EXERCÍCIO 07
 function retornaNNumerosPares(n) {
-  
+  const novoArray = []
+  for (let number = 0; novoArray.length < n; number++) {
+    if (number % 2 === 0) {
+      novoArray.push(number)
+    }
+  }
+  return novoArray
+
 }
 retornaNNumerosPares()
 
@@ -85,9 +92,9 @@ function checaTriangulo(a, b, c) {
   // return 'Escaleno'
   // return 'Equilátero'
   // return 'Isósceles'
-  if(a === b && b === c){
+  if (a === b && b === c) {
     return 'Equilátero'
-  } else if((a !== b) && (a !== c) && (b !== c) ){
+  } else if ((a !== b) && (a !== c) && (b !== c)) {
     return 'Escaleno'
   } else {
     return 'Isósceles'
@@ -103,48 +110,85 @@ function comparaDoisNumeros(num1, num2) {
   //   maiorDivisivelPorMenor: Y,
   //   diferenca: Z
   // }
-  if(num1 > num2){
+  if (num1 > num2) {
     const objeto = {
       maiorNumero: num1,
-      maiorDivisivelPorMenor: num1 % num2 ===0,
+      maiorDivisivelPorMenor: num1 % num2 === 0,
       diferenca: num1 - num2
     }
     return objeto
-  } else if(num1 < num2){
+  } else if (num1 < num2) {
     const objeto = {
       maiorNumero: num2,
-      maiorDivisivelPorMenor: num2 % num1 ===0,
+      maiorDivisivelPorMenor: num2 % num1 === 0,
       diferenca: num2 - num1
-  } 
-  return objeto
-} else {
-  const objeto = {
-    maiorNumero: num1,
-    maiorDivisivelPorMenor: num2 % num1 ===0,
-    diferenca: num2 - num1
-} 
-return objeto
-}
+    }
+    return objeto
+  } else {
+    const objeto = {
+      maiorNumero: num1,
+      maiorDivisivelPorMenor: num2 % num1 === 0,
+      diferenca: num2 - num1
+    }
+    return objeto
+  }
 
 }
-// comparaDoisNumeros()
+comparaDoisNumeros()
 
 
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
 
+  let menor = Infinity
+  let maior = -Infinity
+  let segundoMenor = Infinity
+  let segundoMaior = -Infinity
+  let novoArray = []
 
+  for (let i of array) {
+    if (i < menor) {
+      menor = i
+    }
+    if (i > maior) {
+      maior = i
+    }
+  }
+
+  for (let i of array) {
+    if (i < segundoMenor && i !== menor) {
+      segundoMenor = i
+    }
+    if (i > segundoMaior && i !== maior) {
+      segundoMaior = i
+    }
+  }
+  novoArray.push(segundoMaior)
+  novoArray.push(segundoMenor)
+
+  return novoArray
 }
 
 // EXERCÍCIO 11
 function ordenaArray(array) {
+  const len = array.length
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (array[j] > array[j + 1]) {
+        let tmp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = tmp
+      }
+    }
+  }
+  return array
 
 }
 
 // EXERCÍCIO 12
 function filmeFavorito() {
-  const filme ={
-    nome:"O Diabo Veste Prada",
+  const filme = {
+    nome: "O Diabo Veste Prada",
     ano: Number(2006),
     diretor: "David Frankel",
     atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
@@ -156,8 +200,8 @@ filmeFavorito()
 // EXERCÍCIO 13
 function imprimeChamada() {
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
-  const filme ={
-    nome:"O Diabo Veste Prada",
+  const filme = {
+    nome: "O Diabo Veste Prada",
     ano: Number(2006),
     diretor: "David Frankel",
     atores: ["Meryl Streep", " Anne Hathaway", " Emily Blunt", " Stanley Tucci"]
@@ -180,8 +224,8 @@ criaRetangulo()
 
 // EXERCÍCIO 15
 function anonimizaPessoa(pessoa) {
- const anonimo = {...pessoa,nome:"ANÔNIMO"}
- return anonimo
+  const anonimo = { ...pessoa, nome: "ANÔNIMO" }
+  return anonimo
 }
 anonimizaPessoa()
 // EXERCÍCIO 16A
