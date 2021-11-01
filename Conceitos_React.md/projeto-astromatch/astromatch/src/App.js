48c6d2a5aa660fd67a8421fe2fd5337d577ee1df
kaio vicente <kaio_vicente09@hotmail.com>
2021-10-31 23:28:25 -0300

    basicamente fiz o projeto mas continua dando uns erros

--- Conceitos_React.md/projeto-astromatch/astromatch/src/App.js
+++ Conceitos_React.md/projeto-astromatch/astromatch/src/App.js
@@ -1,9 +1,54 @@
 import{
-  HomePage
+  HomePage, MatchesPage,
 } from './pages';
+import { useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from './baseUrl.js/baseUrl';
+
+
 function App() {
+  const [pageName, setPageName] = useState("home");
+
+  const getPagina = () => {
+    switch(pageName) {
+      case "home":
+        return <HomePage/>
+        case "matches":
+          return <MatchesPage />
+          default:
+            return <HomePage/>
+    }
+  }
+
+  const mudarPagina =() =>{
+    if(pageName === "home") {
+      setPageName("matches");
+    } else {
+      setPageName ("home");
+    }
+
+  }
+
+  const clearMatches = ()=> {
+    const URL = `${BASE_URL}/clear`
+
+    axios.put(URL)
+    .then(()=>{
+      alert("Limpeza feita")
+      setPageName("home")
+    })
+    .catch((err)=>{
+      console.log(err.response)
+    })
+  }
+
+
   return (
-  <HomePage />
+  <div>
+    {getPagina()}
+    <button onClick= {mudarPagina} >{pageName === "home" ? "Ir para Matches" : "Ir para Home" }</button>
+    <button onClick= {clearMatches}>Limpar Matches</button>
+  </div>
   );
 }
 
---
+++ Conceitos_React.md/projeto-astromatch/astromatch/src/pages/matches/styles.js
@@ -0,0 +1,29 @@
+import styled from'styled-components';
+
+export const PagContainer = styled.div`
+width: 100vw;
+display: flex;
+justify-content: center;
+height: 80vh;
+`
+export const HomeContainer = styled.div`
+border: 1px solid black;
+width: 40%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+padding: 15px;
+height: 100%;
+`;
+
+export const ListMathes = styled.div`
+border: 1px solid black;
+display : flex ;
+justify-content: space-between;
+width: 80%;
+`;
+
+export const Avatar = styled.img`
+max-width: 50px;
+`
